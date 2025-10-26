7c805ef32fb233d91e40d1a7e2d75a2ddbf13f91
nhatcomnhom <nhatcomnhom@gmail.com>
2025-10-27 04:07:44 +0900

    fix cors

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,8 @@
-import os from 'os';
-
-function getLocalIPs(): string[] {
-  const interfaces = os.networkInterfaces();
-  const ips: string[] = [];
-
-  for (const name of Object.keys(interfaces)) {
-    const nets = interfaces[name];
-    if (!nets) continue; // ✅ skip undefined interfaces
-
-    for (const net of nets) {
-      if (net.family === 'IPv4' && !net.internal) {
-        ips.push(`http://${net.address}:3000`);
-      }
-    }
-  }
-
-  return ips;
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  /* config options here */
+  
 }
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: [
-    'http://localhost',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    process.env.IP_HOST_URL,
-    ...getLocalIPs(),
-  ],
-};
-
-console.log('✅ NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
-
-console.log('✅ Allowed dev origins:', nextConfig.allowedDevOrigins);
-
-export default nextConfig;
+ 
+export default nextConfig
